d0fc316d3c6db98ceff647eb53c12becaab8a488
tontake <graceglory591@gmail.com>
2019-12-25 17:24:09 -0800

    added first iteration

--- views/header.php
+++ views/header.php
@@ -2,29 +2,61 @@
 echo '<!DOCTYPE html>
 <html>
 <head><title> KAPULA</title>
+<link href="https://fonts.googleapis.com/css?family=Lato|Russo+One&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel ="stylesheet" href="css/style.css" />
+
 
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </head>
 <body>
-<header>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">KAPULA</a>
+<header class="sec1">
+<nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+  <a class="navbar-brand" href="#"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#">About</a>
-      <a class="nav-item nav-link" href="#">Product</a>
-      <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Contact</a>
+    <div class="navbar-nav ml-auto px-4">
+      <a class="nav-item nav-link active mx-2" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link  mx-2" href="#">About</a>
+      <a class="nav-item nav-link  mx-2" href="#">Product</a>
+      <a class="nav-item nav-link " href="#" ><button class=" btn btn-primary">Sign In</button></a>
     </div>
   </div>
 </nav>
+<div class="sec2">
+<div class="container">
+  <!-- Stack the columns on mobile by making one full-width and the other half-width -->
+  <div class="row">
+    <div class="col-md-7 col-xs-12">
+    <div class ="mantra "><h2>Eat, Enjoy and Relax  </h2></div>
+     <div class ="mantra"><h4>With </h4></div>
+     <div class ="mantrabigger"><h1><strong> KAPULA</strong></h1></div>
+    
+    </div>
+    <div class=" col-md-5 col-xs-12"><img class="img-responsive" src ="images/nyathi.jpg"></div>
+  </div>
+  <!--writing discount area-->
+  <div class="row">
+    <div class="col-md-7 offset-md-2"><h4>Get 3 % discount from a click </h4></div>
+  
+  <div class="col-md-3"></div>
+  </div>
+  <!--end of discount-->
 
+  <!--writing buy now area-->
+  <div class="row">
+    <div class="col-md-6 offset-md-3"> <button class="buynow ml-2">Buy Now</button></div>
+  
+  <div class="col-md-3"></div>
+  </div>
+  <!--end of buy now-->
+  </div>
+
+</div>
 </header>
 </body>
 
