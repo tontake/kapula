f2e08f079971b294ec7511ad5cc71727b04cce1d
tontake <graceglory591@gmail.com>
2019-12-27 12:27:10 -0800

    testing heroku

--- views/header.php
+++ views/header.php
@@ -1,6 +1,7 @@
 <?php
 echo '<!DOCTYPE html>
 <html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <head><title> KAPULA</title>
 <link href="https://fonts.googleapis.com/css?family=Lato|Russo+One&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
